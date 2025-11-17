e9309482c62e370477166f5fabd0e0e58cb660ce
Bruggles718 <bkarmin718@gmail.com>
2025-11-17 08:28:01 -0500

    saving current server state

--- do_commit.sh
+++ do_commit.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd ~/btrfcccobmc_server
 git add --all
 git commit -m "saving current server state"
 git push
---
+++ watchdog_runner.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+while true; do
+  ./start.sh
+  echo "Restarting in 5 seconds..."
+  sleep 5
+done
